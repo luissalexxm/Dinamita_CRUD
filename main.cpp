f48a192bf6d7a01b3a189335083a5f0a0c9d2347
luissalexxm <a24310139@ceti.mx>
2024-11-26 14:57:15 -0600

    Initial commit: Estructura Básica

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct User {
+    int id;
+    string name;
+};
+
+vector<User> users;
+
+void createUser();
+void readUsers();
+void updateUser();
+void deleteUser();
+
+int main() {
+    int choice;
+
+    do {
+        cout << "\nCRUD Menu:\n";
+        cout << "1. Create User\n";
+        cout << "2. Read Users\n";
+        cout << "3. Update User\n";
+        cout << "4. Delete User\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: createUser(); break;
+            case 2: readUsers(); break;
+            case 3: updateUser(); break;
+            case 4: deleteUser(); break;
+            case 5: cout << "Exiting...\n"; break;
+            default: cout << "Invalid option. Try again.\n";
+        }
+    } while (choice != 5);
+
+    return 0;
+}
